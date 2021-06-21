bad22528fa7017ccea678645262e5a8cae873e94
Muhammad Muaaz <muaaznaeemnxb@gmail.com>
2021-06-21 22:12:14 +0500

    used code first with employee model

---
+++ Empty/Models/Nextbridge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Empty.Models
+{
+    public class Nextbridge:DbContext
+    {
+        public Nextbridge():base("SchoolDbContext")
+        {
+         }
+        public DbSet<Employee> employees { get; set; }
+    }
+}
